47693f79274fff2afe6667b31d780ef2dc29ae75
sbbeez <senthil.g.balaji@gmail.com>
2021-10-03 00:32:13 +0530

    addition in todo

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ Please feel free to drop a feedback mail to g.senthilbalaji@gmail.com
 
 # best-practices-links
 1. [Best practices for node](https://github.com/goldbergyoni/nodebestpractices/blob/master/README.md)
+2. [API documentation](https://swagger.io/blog/api-documentation/create-compelling-easy-to-use-api-documentation/)
+
 # roadmap
 1. Best practices to document and add comments
 2. ...much more... see some more in TODO
--- src/api/project/objectConstructor.ts
+++ src/api/project/objectConstructor.ts
@@ -1,8 +1,8 @@
 /**
- * This module accepts express request object and constructs required params for each route
- * Each method corresponds to different routes
- * Acts as a middleware to construct the object required by each object
- * Once the object is constructed from request, invokes other middlewares if any
+ * This module accepts express request object and constructs required params for each route.
+ * Each method corresponds to different routes.
+ * Acts as a middleware to construct the object required by each object.
+ * Once the object is constructed from request, invokes other middlewares if any.
  */
 // external modules installed using npm
 import * as Express from "express";
